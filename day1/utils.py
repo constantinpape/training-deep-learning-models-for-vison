874ee0f6e7db8136ae94734da71c6362eaf25b51
Constantin Pape <c.pape@gmx.net>
2020-09-11 14:42:14 +0200

    Add full train funvtion and simple cnn to utils

--- day1/utils.py
+++ day1/utils.py
@@ -5,12 +5,18 @@ from itertools import product
 
 import numpy as np
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
 from torch.utils.data import Dataset
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.utils.tensorboard import SummaryWriter
+
 import sklearn.metrics as metrics
 from sklearn.model_selection import train_test_split
 
 from imageio import imread
-from tqdm import tqdm
+from tqdm import tqdm, trange
 
 
 #
@@ -136,6 +142,26 @@ def make_cifar_test_dataset(cifar_dir, transform=None):
 
     dataset = DatasetWithTransform(images, labels, transform=transform)
     return dataset
+
+
+#
+# checkpoints
+#
+
+def save_checkpoint(model, optimizer, epoch, save_path):
+    torch.save({
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict()
+    }, save_path)
+
+
+def load_checkpoin(save_path, model, optimizer):
+    checkpoint = torch.load(save_path)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    epoch = checkpoint['epoch']
+    return model, optimizer, epoch
 
 
 #
@@ -290,6 +316,50 @@ def validate(model, loader, loss_function,
     return predictions, labels
 
 
+def run_cifar_training(model, optimizer,
+                       train_loader, val_loader,
+                       device, name, n_epochs):
+    """ Complete training logic
+    """
+
+    best_accuracy = 0.
+
+    loss_function = nn.NLLLoss()
+    loss_function.to(device)
+
+    scheduler = ReduceLROnPlateau(optimizer,
+                                  mode='max',
+                                  factor=0.5,
+                                  patience=1)
+
+    checkpoint_path = f'best_checkpoint_{name}.tar'
+    log_dir = f'runs/{name}'
+    tb_logger = SummaryWriter(log_dir)
+
+    for epoch in trange(n_epochs):
+        train(model, train_loader, loss_function, optimizer,
+              device, epoch, tb_logger=tb_logger)
+        step = (epoch + 1) * len(train_loader)
+
+        pred, labels = validate(model, val_loader, loss_function,
+                                device, step,
+                                tb_logger=tb_logger)
+        val_accuracy = metrics.accuracy_score(labels, pred)
+        scheduler.step(val_accuracy)
+
+        # otherwise, check if this is our best epoch
+        if val_accuracy > best_accuracy:
+            # if it is, save this check point
+            best_accuracy = val_accuracy
+            save_checkpoint(model, optimizer, epoch, checkpoint_path)
+
+    return checkpoint_path
+
+
+#
+# visualisation functionality
+#
+
 def make_confusion_matrix(labels, predictions, categories, ax):
     cm = metrics.confusion_matrix(labels, predictions)
 
@@ -312,3 +382,47 @@ def make_confusion_matrix(labels, predictions, categories, ax):
     plt.tight_layout()
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
+
+
+#
+# models
+#
+
+class SimpleCNN(nn.Module):
+    def __init__(self, n_classes):
+        super().__init__()
+        self.n_classes = n_classes
+
+        # the convolutions
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=12, kernel_size=5)
+        self.conv2 = nn.Conv2d(in_channels=12, out_channels=24, kernel_size=3)
+        self.pool = nn.MaxPool2d(2, 2)
+
+        # the fully connected part of the network
+        # after applying the convolutions and poolings, the tensor
+        # has the shape 24 x 6 x 6, see below
+        self.fc = nn.Sequential(
+            nn.Linear(24 * 6 * 6, 120),
+            nn.ReLU(),
+            nn.Linear(120, 60),
+            nn.ReLU(),
+            nn.Linear(60, self.n_classes)
+        )
+        self.activation = nn.LogSoftmax(dim=1)
+
+    def apply_convs(self, x):
+        # input image has shape 3 x  32 x 32
+        x = self.pool(F.relu(self.conv1(x)))
+        # shape after conv: 12 x 28 x 28
+        # shape after pooling: 12 x 14 X 14
+        x = self.pool(F.relu(self.conv2(x)))
+        # shape after conv: 24 x 12 x 12
+        # shape after pooling: 24 x 6 x 6
+        return x
+
+    def forward(self, x):
+        x = self.apply_convs(x)
+        x = x.view(-1, 24 * 6 * 6)
+        x = self.fc(x)
+        x = self.activation(x)
+        return x
